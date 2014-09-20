723206eced13e7e217de5ed1c2f9695a9fae8ca2
Kevin Lamping <klamping@gmail.com>
2014-09-20 15:18:43 -0500

    added template for exercises

--- index.js
+++ index.js
@@ -1,3 +1,6 @@
+var open = require('open');
 var connect = require('connect');
 var serveStatic = require('serve-static');
 connect().use(serveStatic(__dirname + '/www/')).listen(8080);
+open('http://localhost:8080');
+console.log('Server running at http://localhost:8080');
